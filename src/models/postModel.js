f929dac23e3e0d176def041ba10ef64e52676e59
cauasptech <caua.martos@sptech.school>
2026-05-08 19:54:49 -0300

    Fix no postModel

--- src/models/postModel.js
+++ src/models/postModel.js
@@ -1,34 +1,34 @@
 var database = require("../database/config");
 
 function listar() {
-    var instrucaoSql = `
+  var instrucaoSql = `
         SELECT p.idpost, p.esporte, p.dtTreino, p.descricao, u.nome
         FROM post p
         JOIN usuario u ON p.fkUsuario = u.idusuario
         ORDER BY p.dtTreino DESC;
     `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
 }
 
 function publicar(esporte, descricao, fkUsuario) {
-    var instrucaoSql = `
+  var instrucaoSql = `
         INSERT INTO post (esporte, dtTreino, descricao, fkUsuario) VALUES ('${esporte}', NOW(), '${descricao}', '${fkUsuario}');
     `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
 }
 
 function contarPorEsporte() {
-    var instrucaoSql = `
+  var instrucaoSql = `
         SELECT esporte, COUNT(*) AS total FROM post GROUP BY esporte;
     `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
 }
 
 module.exports = {
-    listar,
-    publicar,
-    contarPorEsporte
+  listar,
+  publicar,
+  contarPorEsporte,
 };
--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -3,6 +3,7 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
 
+    // JOIN com academia para retornar o nome da academia
     var instrucaoSql = `
         SELECT u.idusuario id, u.nome, u.email, u.genero, a.nome AS nome_academia FROM usuario u LEFT JOIN academia a ON u.fkAcademia = a.idacademia WHERE u.email = '${email}' AND u.senha = '${senha}';
     `;
@@ -14,6 +15,7 @@ function autenticar(email, senha) {
 function cadastrar(nome, email, senha, genero, nomeAcademia) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, genero, nomeAcademia);
 
+    // subquery para buscar o id da academia pelo nome
     var instrucaoSql = `
         INSERT INTO usuario (nome, email, senha, genero, fkAcademia) VALUES ('${nome}', '${email}', '${senha}', '${genero}', (SELECT idacademia FROM academia WHERE nome = '${nomeAcademia}' LIMIT 1));
     `;
@@ -22,7 +24,17 @@ function cadastrar(nome, email, senha, genero, nomeAcademia) {
     return database.executar(instrucaoSql);
 }
 
+// contar usuários por gênero
+function contarPorGenero() {
+    var instrucaoSql = `
+        SELECT genero, COUNT(*) AS total FROM usuario GROUP BY genero;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
 module.exports = {
     autenticar,
-    cadastrar
+    cadastrar,
+    contarPorGenero
 };
